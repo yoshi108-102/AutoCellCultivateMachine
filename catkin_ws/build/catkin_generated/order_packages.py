d3e5c86c5b217c8d8cf7a4696d0f671e09043152
tw017 <ginyoku102@gmail.com>
2024-10-17 16:47:36 +0900

    lidarのmoveitでの認識スクリプトを作成途中

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/tw017/Desktop/PipetteDetect/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/tw017/Desktop/PipetteDetect/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/tw017/Desktop/PipetteDetect/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/tw017/ros/livox/devel;/home/tw017/Desktop/PipetteDetect/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/tw017/ros/livox/devel;/home/tw017/Desktop/PipetteDetect/catkin_ws/devel;/opt/ros/noetic' != '' else []
