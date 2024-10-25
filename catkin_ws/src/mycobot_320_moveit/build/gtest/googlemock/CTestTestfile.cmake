86594cdf877b72e23a7791233a3338b87fffcf3e
tw017 <ginyoku102@gmail.com>
2024-10-25 14:43:25 +0900

    一度全て消す

--- catkin_ws/src/mycobot_320_moveit/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/tw017/Desktop/PipetteDetect/catkin_ws/src/mycobot_320_moveit/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
