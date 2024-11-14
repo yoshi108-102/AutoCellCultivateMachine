6019985d7ff3beee1d163a2efdc697c950de312d
tw017 <ginyoku102@gmail.com>
2024-11-14 14:17:52 +0900

    gitignore add build

--- catkin_ws/src/cobotta_moveit/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tw017/Desktop/PipetteDetect/catkin_ws/src/cobotta_moveit
-# Build directory: /home/tw017/Desktop/PipetteDetect/catkin_ws/src/cobotta_moveit/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
